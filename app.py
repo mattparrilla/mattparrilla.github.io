8d1e5982912230afe78a0e98799fe53e21066521
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-05-09 08:31:54 -0400

    Get inertial navigation post flaskified

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/post/<title>")
+def post(title):
+    return render_template("{}.html".format(title))
