41e806d55e1157f25d26150c1f7ae7587ab11247
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-05-09 08:48:29 -0400

    Flaskify http2 and wxgif posts

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# TODO put required info, but not content, in a JSON object for each post
+# and pass that to the template. Can perform validation in the view function
+# to make sure needed info is included (eg. description, title, image)
+
+# TODO update static paths
+
+# TODO social cards
+
+# TODO freeze pages, likely just render app at a given view
+
 
 @app.route("/")
 def index():
