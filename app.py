934297743b1712bf8186f3fb52cd11ebe7d8adc6
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-05-11 19:52:10 -0400

    Hack flask url to generate index.html for posts

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/post/<title>")
+@app.route("/post/<title>/index.html")
 def post(title):
     post_properties = {}
     with open("posts.json", "r") as f:
