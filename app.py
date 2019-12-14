fde33d93d39473c229cc9ac34dbfd0e54e6c0e45
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-12-13 20:14:41 -0500

    Lay out main map page and detail

--- app.py
+++ app.py
@@ -34,6 +34,30 @@ def post(title):
     return render_template("{}.html".format(title), **post_properties)
 
 
+@app.route("/maps/<title>/index.html")
+def map(title):
+    post_properties = {}
+    with open("posts.json", "r") as f:
+        posts = json.load(f)
+        post_properties = posts[title]
+    post_properties["url"] = request.path
+
+    if not post_properties.get("title", False):
+        raise ValueError("Missing title entry for {}".format(title))
+    if not post_properties.get("description", False):
+        raise ValueError("Missing description entry for {}".format(title))
+
+    return render_template("{}.html".format(title), **post_properties)
+
+
+@freezer.register_generator
+def map():
+    with open("posts.json", "r") as f:
+        posts = json.load(f)
+        for title in posts.keys():
+            yield {"title": title}
+
+
 @freezer.register_generator
 def post():
     with open("posts.json", "r") as f:
