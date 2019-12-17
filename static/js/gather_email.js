9cc30d14c3e0910f653a10fa80de8efe7ddbb8d4
Matthew Parrilla <matthew.parrilla@gmail.com>
2019-12-16 20:52:40 -0500

    Submit email via form

---
+++ static/js/gather_email.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", () => {
+  var form = document.getElementById("email_form");
+
+  function sendData() {
+    var XHR = new XMLHttpRequest();
+
+    // Bind the FormData object and the form element
+    var FD = new FormData(form);
+
+
+    // Set up our request
+    XHR.open("POST", "https://5r16enkcob.execute-api.us-east-1.amazonaws.com/api/");
+    XHR.setRequestHeader("content-type", "application/json");
+
+    // The data sent is what the user provided in the form
+    var json = {};
+    FD.forEach((value, key) => { json[key] = value; });
+    XHR.onreadystatechange = function () {
+      var onSubmit = document.getElementById("email_response");
+      if (XHR.readyState === 4) {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
+        if (XHR.status === 200) {
+          document.getElementById('email').value = '';
+          onSubmit.innerHTML = "Sent";
+        } else {
+          onSubmit.innerHTML = "Oops. Something is wrong.";
+        }
+      }
+    };
+    XHR.send(JSON.stringify(json));
+  }
+  // ...and take over its submit event.
+  form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      console.log('yep');
+
+      sendData();
+  });
+});
